6283bcfb131d9118db4017e0434590f8c802d923
DylanCahuMDS <dylan.cahu@my-digital-school.org>
2022-10-24 13:41:05 +0200

    debug img location

--- Mastermind_Interface/Mastermind_Interface/Form1.Designer.cs
+++ Mastermind_Interface/Mastermind_Interface/Form1.Designer.cs
@@ -46,6 +46,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.Color.SandyBrown;
             this.ClientSize = new System.Drawing.Size(1197, 532);
             this.Controls.Add(this.Submit);
